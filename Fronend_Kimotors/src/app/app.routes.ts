8d086dd731a6133a87ec2f720829431968d773a8
Jesus Emilio <jeosoriop@ufpso.edu.co>
2025-03-01 01:19:08 -0500

    SOA-7: primer cambio de la pagina principal

--- Fronend_Kimotors/src/app/app.routes.ts
+++ Fronend_Kimotors/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
 
 export const routes: Routes = [
+  { path: '', component: MainPageComponent,pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: '**', redirectTo: '/login' }
+  { path: '**', redirectTo: '' }
 ];
