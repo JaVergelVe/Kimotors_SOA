bc5050ae067a0e421465a29e7baecf1b1723744a
Jesus Emilio <jeosoriop@ufpso.edu.co>
2025-03-06 23:25:32 -0500

    SOA-7: creacio componente versus y un poco de adelanto

---
+++ Fronend_Kimotors/src/app/versus/versus.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { MainPageComponent } from '../components/main-page/main-page.component';
+
+@Component({
+  selector: 'app-versus',
+  imports: [RouterLink],
+  templateUrl: './versus.component.html',
+  styleUrl: './versus.component.css'
+})
+export class VersusComponent {
+  
+}
