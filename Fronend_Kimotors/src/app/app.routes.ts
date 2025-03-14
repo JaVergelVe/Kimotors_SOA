f277704e63d4d449cf7e8d10d30db0a508f5b67c
JaVergelVe <javergelve@ufpso.edu.co>
2025-03-14 15:48:45 -0500

    SOA-8: Se agrego el boton para volver al inicio en la interfaz de register y login, ademas de agregar otro boton en el login para reestablecer contraseña

--- Fronend_Kimotors/src/app/app.routes.ts
+++ Fronend_Kimotors/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { VersusComponent } from './versus/versus.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 
 export const routes: Routes = [
   { path: '', component: MainPageComponent,pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'versus', component: VersusComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: '**', redirectTo: '' }
 ];
