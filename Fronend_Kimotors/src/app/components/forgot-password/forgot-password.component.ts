b8dcaa36f2ee20fbf7be6fecdb481c7c48350ce1
JaVergelVe <javergelve@ufpso.edu.co>
2025-03-15 15:51:56 +0100

    SOA-10: Se agrego un campo y una funcion para comfirmar contraseña en la interfaz de reestablecer contraseña

--- Fronend_Kimotors/src/app/components/forgot-password/forgot-password.component.ts
+++ Fronend_Kimotors/src/app/components/forgot-password/forgot-password.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, AbstractControl, ValidationErrors } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
@@ -21,7 +21,24 @@ export class ForgotPasswordComponent implements OnInit {
     this.forgotPasswordForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       newPassword: ['', [Validators.required, Validators.minLength(6)]], 
-    });
+      confirmNewPassword: ['', [Validators.required, Validators.minLength(6)]],
+    }, {
+      validators: this.passwordMatchValidator
+    }
+  ); 
+  }
+
+  // Función para validar que las contraseñas coincidan
+  passwordMatchValidator(control: AbstractControl): ValidationErrors | null {
+    const password = control.get('password')?.value;
+    const confirmPassword = control.get('confirmPassword')?.value;
+
+    if (password !== confirmPassword) {
+      control.get('confirmPassword')?.setErrors({ passwordMismatch: true });
+      return { passwordMismatch: true };
+    } else {
+      return null;
+    }
   }
 
   get f() {
