5c994ead5e4b62c3f427a7052662e923837d7236
JaVergelVe <javergelve@ufpso.edu.co>
2025-03-15 21:06:35 -0500

    SOA-13: Se arreglaron las dependencias y versiones del proyecto ademas de configurar inicialmente el firebase

---
+++ Fronend_Kimotors/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyD4yHO0Kv4PopPlGicxRF2PcX5em1zoTNU",
+        authDomain: "kimotors.firebaseapp.com",
+        projectId: "kimotors",
+        storageBucket: "kimotors.firebasestorage.app",
+        messagingSenderId: "219455797003",
+        appId: "1:219455797003:web:62ce9d91ba67512049d40e",
+        measurementId: "G-T8HNK4NKL5",
+    }
+}
--- Fronend_Kimotors/src/main.ts
+++ Fronend_Kimotors/src/main.ts
@@ -1,10 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { environment } from './environments/environment';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes)
-  ]
-});
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
+  ],
+}).catch((err) => console.error(err));
