9aa6e5a96e6202f0d4d81f0b648fb91b5bbc6b6c
Emiliano16161616 <jemiliosp24@gmail.com>
2025-03-17 17:31:29 -0500

    SOA-12: primer avance de la seccion de detalles de motos

--- Fronend_Kimotors/src/app/app.routes.ts
+++ Fronend_Kimotors/src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { RegisterComponent } from './components/register/register.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { VersusComponent } from './components/versus/versus.component';
+import { VistaMotoComponent } from './components/vista-moto/vista-moto.component';
 
 export const routes: Routes = [
   { path: '', component: MainPageComponent, pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'versus', component: VersusComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'vista-moto', component: VistaMotoComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: '**', redirectTo: '' }
 ];
