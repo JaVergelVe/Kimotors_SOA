6045f88c4f8af8277eb89b6638d7ed4aa173ff91
Jesus Emilio <jeosoriop@ufpso.edu.co>
2025-03-20 15:40:41 -0500

    SOA-7: encabezado terminado

--- Fronend_Kimotors/src/app/app.routes.ts
+++ Fronend_Kimotors/src/app/app.routes.ts
@@ -5,12 +5,14 @@ import { MainPageComponent } from './components/main-page/main-page.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { VersusComponent } from './components/versus/versus.component';
 import { VistaMotoComponent } from './components/vista-moto/vista-moto.component';
+import { EncabezadoComponent } from './components/encabezado/encabezado.component';
 
 export const routes: Routes = [
   { path: '', component: MainPageComponent, pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'versus', component: VersusComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'encabezado', component: EncabezadoComponent },
   { path: 'vista-moto', component: VistaMotoComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: '**', redirectTo: '' }
