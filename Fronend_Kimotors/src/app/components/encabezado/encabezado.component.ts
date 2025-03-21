d291b93be96f1c8ae0d2f5a3d591482dfbbda3bf
Jesus Emilio <jeosoriop@ufpso.edu.co>
2025-03-21 17:40:53 -0500

    SOA-12: pagina de vista-moto finalizada

--- Fronend_Kimotors/src/app/components/encabezado/encabezado.component.ts
+++ Fronend_Kimotors/src/app/components/encabezado/encabezado.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-encabezado',
-  imports: [],
+  imports: [RouterLink,CommonModule],
   templateUrl: './encabezado.component.html',
   styleUrl: './encabezado.component.css'
 })
