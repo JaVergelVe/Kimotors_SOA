164f48a66f96b735a61905e3af00e7d05c3f295c
Jesus Emilio <jeosoriop@ufpso.edu.co>
2025-03-28 23:09:40 -0500

    SOA-19: cambios en el main-page y componente encabezado en todas las rutas

--- Fronend_Kimotors/src/app/app.component.ts
+++ Fronend_Kimotors/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { EncabezadoComponent } from './components/encabezado/encabezado.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule,EncabezadoComponent],
   templateUrl:'./app.component.html'
 
 })
--- Fronend_Kimotors/src/app/components/main-page/main-page.component.ts
+++ Fronend_Kimotors/src/app/components/main-page/main-page.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { EncabezadoComponent } from '../encabezado/encabezado.component';
+import { TarjetaMotoComponent } from '../tarjeta-moto/tarjeta-moto.component';
 
 @Component({
   selector: 'app-main-page',
-  imports: [RouterLink,CommonModule],
+  imports: [RouterLink,CommonModule, EncabezadoComponent,TarjetaMotoComponent],
   standalone: true,
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
